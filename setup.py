f8e81307681fecbbe630744f90ce68ddaf6f34ec
rodrigo <rodrigorodriguescosta@gmail.com>
2016-05-03 09:51:19 -0300

    Primeiro commit

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.1',
+    version='1.2',
 
     description='Projeto que contem rotinas uteis para os servicos do projeto Sigeflex',
     # long_description=long_description,
