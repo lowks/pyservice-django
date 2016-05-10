575e3994ed9b30d0252f50ec647d7e6abc14e962
rodrigo <rodrigorodriguescosta@gmail.com>
2016-05-10 20:47:59 -0300

    criacao da classe djservice

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_microservices',
-    version='1.0.2',
+    version='1.0.3',
     url='https://github.com/bittecnologia/django-microservices',
     author='Rodrigo Rodrigues',
     author_email='rodrigorodriguescosta@gmail.com',
